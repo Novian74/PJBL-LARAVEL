c7dc367816d684449179f768fedf896e79d48248
Novian24 <109934273+Novian74@users.noreply.github.com>
2022-12-06 07:43:11 +0700

    Update web.php

--- laravel-restosmk/routes/web.php
+++ laravel-restosmk/routes/web.php
@@ -61,8 +61,6 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
     Route::group(['middleware' => ['Ceklogin:manager']], function () {
         Route::resource('kategori', KategoriController::class);
         Route::resource('menu', MenuController::class);
-        Route::resource('order', OrderController::class);
-        Route::resource('order', OrderController::class);
         Route::resource('pelanggan', PelangganController::class);
         Route::get('select', [MenuController::class, 'select']);
         Route::post('postmenu/{id}', [MenuController::class, 'update']);
